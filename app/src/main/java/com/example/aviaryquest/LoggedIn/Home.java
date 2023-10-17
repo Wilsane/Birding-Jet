7265e7505fa805d3b47c63e6b79760b2f6645e44
Tshedza Mathada <ST10131676@rcconnect.edu.za>
2023-10-17 22:59:00 +0000

    hotsopt update

--- app/src/main/java/com/example/aviaryquest/LoggedIn/Home.java
+++ app/src/main/java/com/example/aviaryquest/LoggedIn/Home.java
@@ -9,8 +9,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.example.aviaryquest.LoggedIn.Settings;
+
+import com.example.aviaryquest.LoggedIn.HomeTasks.Hotspots;
 import com.example.aviaryquest.LoggedIn.HomeTasks.Nearby;
 import com.example.aviaryquest.R;
 
@@ -18,6 +22,8 @@ import com.example.aviaryquest.R;
 public class Home extends Fragment {
 
     FrameLayout frameLayout;
+    Button btn_nearby,btn_hotspots;
+    Fragment fragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,13 +32,41 @@ public class Home extends Fragment {
         View view= inflater.inflate(R.layout.fragment_home, container, false);
 
         frameLayout=view.findViewById(R.id.home_frameLayout);
+        btn_nearby=view.findViewById(R.id.btn_nearby);
+        btn_hotspots=view.findViewById(R.id.btn_hotspots);
 
+        //When the nearby button is pressed
+        btn_nearby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment=new Nearby();
+                SwitchFragments(fragment);
 
-        Nearby nearby=new Nearby();
-        SwitchFragments(nearby);
+                btn_nearby.setTextColor(getResources().getColor(R.color.white));
+                btn_nearby.setBackground(getResources().getDrawable(R.drawable.selected_home_top_navbar));
+                btn_hotspots.setBackground(getResources().getDrawable(R.drawable.not_selected_home_top_navbar));
+                btn_hotspots.setTextColor(getResources().getColor(R.color.limeGreen));
+            }
+        });
+
+        //When hotspots button is pressed
+        btn_hotspots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment=new Settings();
+                SwitchFragments(fragment);
+
+                btn_hotspots.setTextColor(getResources().getColor(R.color.white));
+                btn_hotspots.setBackground(getResources().getDrawable(R.drawable.selected_home_top_navbar));
+                btn_nearby.setBackground(getResources().getDrawable(R.drawable.not_selected_home_top_navbar));
+                btn_nearby.setTextColor(getResources().getColor(R.color.limeGreen));
+            }
+        });
+
+        //Default fragment
+        SwitchFragments(new Nearby());
         return  view;
     }
-
 
     private void SwitchFragments(Fragment frag){
         FragmentManager fm = getChildFragmentManager();
